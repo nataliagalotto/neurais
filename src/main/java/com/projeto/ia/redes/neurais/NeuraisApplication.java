44e2da46b1446ee8c6445337783c3d8951c8638c
nalmeida <nalmeida@uolinc.com>
2020-03-07 12:47:36 -0300

    first commit

---
+++ src/main/java/com/projeto/ia/redes/neurais/NeuraisApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.ia.redes.neurais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuraisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuraisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/ia/redes/neurais/NeuraisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.ia.redes.neurais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuraisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
